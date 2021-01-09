df852794c08c7cf8dbf346eee659787ecd313978
Eduardo Costa <eduardocostaprofessor@gmail.com>
2021-01-09 17:59:25 -0300

    Add  a basic crud

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
+
 
 const app = express();
 
@@ -11,19 +12,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  const { title } = request.query;
+
+  console.log(title);
+  const results = title
+    ? repositories.filter(repository => repository.title.toLocaleLowerCase().includes(title.toLocaleLowerCase()))
+    : repositories;
+
+  return response.json(results);
 });
 
+
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  //recupera os dados vindos do front-end
+  const { id } = request.params;
+  const { title, url, techs, likes } = request.body;
+
+  //procura o índice do projeto
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    response.status(400).json({ error: "Repository not Found" })
+  }
+
+  //pega os dados alterados no front-end
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex((repository => repository.id == id));
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  //It`s recomended status code 204 for a empty response!
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
